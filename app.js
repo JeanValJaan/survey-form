80c45b1ba9506a33c9fa808c08b5610309b5b956
cihan orhan <cihanorhan567@gmail.com>
2021-03-10 22:51:53 +0300

    made sone changes

---
+++ app.js
@@ -0,0 +1,11 @@
+// // eslint-disable-next-line linebreak-style
+// console.log('Hello,  new World!');
+// document.write('Hello <br>');
+// document.write('Jean');
+// console.log('Hello:\nClarusway');
+// console.log('Clarusway');
+// alert('Hello\nClarusway!');
+// // eslint-disable-next-line no-restricted-globals
+// confirm('Hello');
+// prompt('yaşınzı giriniz', 35);
+document.body.innerHTML = '<h1> Clarusway Rocks</h1>';
