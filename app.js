23d29b8d2c8c2aa687774d7906ebf4cbcad00331
Jean Val Jaan <jeanvaljaan@gmail.com>
2021-03-15 20:35:30 +0300

    made some changes

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 // // eslint-disable-next-line linebreak-style
-// console.log('Hello,  new World!');
-// document.write('Hello <br>');
-// document.write('Jean');
-// console.log('Hello:\nClarusway');
-// console.log('Clarusway');
-// alert('Hello\nClarusway!');
-// // eslint-disable-next-line no-restricted-globals
-// confirm('Hello');
-// prompt('yaşınzı giriniz', 35);
-document.body.innerHTML = '<h1> Clarusway Rocks</h1>';
-console.time
-console.log('messge')
-console.info('some info')
+// // console.log('Hello,  new World!');
+// // document.write('Hello <br>');
+// // document.write('Jean');
+// // console.log('Hello:\nClarusway');
+// // console.log('Clarusway');
+// // alert('Hello\nClarusway!');
+// // // eslint-disable-next-line no-restricted-globals
+// // confirm('Hello');
+// // prompt('yaşınzı giriniz', 35);
+// // document.body.innerHTML = '<h1> Clarusway Rocks</h1>';
+// // console.time
+// // console.log('messge')
+// // console.info('some info')
+// // 13032021
+// // console.log('Selam');
+// // var a = 3;
+// // console.log(a);
 
+// // a = 4;
+// // console.log(a);
+// // var b;
+// // console.log(typeof b);
+// // b = 6;
+// // console.log(b);
+// // console.log(typeof b);
+// // var a = 10;
+// // {
+// //     let b = 3;
+// //     console.log(b);
+// // }
+// // console.log({a});
+// // console.log({b});
+// // func1();
+
+// // function func1() {
+//   // eslint-disable-next-line no-constant-condition
+// //   if (true) {
+//     // eslint-disable-next-line vars-on-top
+//     // eslint-disable-next-line no-var
+//     // eslint-disable-next-line vars-on-top
+//     // eslint-disable-next-line no-var
+//     // eslint-disable-next-line vars-on-top
+//     // eslint-disable-next-line no-var
+//     // eslint-disable-next-line vars-on-top
+//     // eslint-disable-next-line no-var
+//     // var c = 55;
+//     // const d = 4;
+//     // console.log(c, d);
+// //   }
+//   // eslint-disable-next-line block-scoped-var
+// //   console.log(c);
+//   // eslint-disable-next-line no-undef
+//   console.log(d);
+// }
+// func1();
+// // var x = 5;
+// // console.log(x);
+// // x = 7;
+// // console.log(x);
+// const a = 'Merhaba';
+// console.log(typeof a);
+// console.log((.1 + .2));
+// console.log(Math.floor(0.1 + 0.2))
+// console.log(Boolean('0'));
